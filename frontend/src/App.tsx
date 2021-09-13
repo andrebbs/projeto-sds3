4ee2dd8975d98b4b33fd0086a98aad81e0859a18
André <andre.bbs@gmail.com>
2021-09-13 09:26:46 -0300

    Basic static componentes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,20 @@
+import NavBar from "componentes/NavBar";
+import DataTable from "componentes/DataTable";
+import Footer from "componentes/Footer";
+
+
 function App() {
   return (
-    <div>
-      <h1>Olá mundo</h1>
-    </div>
-   );
+    <>
+      <NavBar />
+      <div className='container'>
+        <h1 className="text-primary">Olá mundo</h1>
+
+        <DataTable />
+      </div>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
